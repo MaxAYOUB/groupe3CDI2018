964a8227a62bd34e6b1d5a35e182afa7aa5fa966
31010-52-05 <maxime.ayoub@gmail.com>
2018-10-16 16:49:46 +0200

    premier commit

---
+++ groupe3/maxime/index.php
@@ -0,0 +1,51 @@
+<?php 
+session_start();
+// var_dump($_GET);
+if (isset($_GET['c']) && isset($_GET['m']) && isset($_GET['a'])){
+	
+	$ctrl = new $_GET['c'];
+	$ctrl->$_GET['m']($_GET['a']);
+
+}else if (isset($_GET['c']) && isset($_GET['m'])){
+	$ctrl = new $_GET['c'];
+	$ctrl->$_GET['m']();
+}else if(isset($_GET['c']) && $_GET['c']!="" && !isset($_GET['m'])){
+	$v=$_GET['c'];
+	$ctrl = new $v;
+	$ctrl->index();
+}else{
+	$v="CtrlContact";
+	$ctrl = new $v;
+	$ctrl->index();
+	ini_set('safe_mode','Off');
+}
+
+function __autoload($class_name = ""){
+	$repertoires = array(
+			"controllers/",
+			"models/",
+			"views/",
+			"system/",
+			"metiers/",
+			"Webservice/",
+			"DAO/",
+			"tests/"
+	);
+	$modules= array(
+		"Modules/Contact/",
+		"Modules/Meteo/",
+		"Modules/fluxRss/",
+		""
+	);
+	foreach($modules as $module){
+		foreach($repertoires as $repertoire){
+				$file = "{$module}{$repertoire}{$class_name}.php";
+			// var_dump($file);
+			
+			if(file_exists($file)){
+				include_once($file);
+			}
+		}
+	}
+}
+?>  

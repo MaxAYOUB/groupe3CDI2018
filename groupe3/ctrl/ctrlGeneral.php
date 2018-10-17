634cc11bc3219feb956a8b5cd2ee883e56fd9a7e
31010-52-05 <maxime.ayoub@gmail.com>
2018-10-17 13:16:15 +0200

    ctrlGeneral

---
+++ groupe3/ctrl/ctrlGeneral.php
@@ -0,0 +1,10 @@
+<?php
+    class ctrlGeneral{
+        private $vue;
+        private $model;
+        
+        function __construct(){
+            $this->vue=new viewGenerale();
+        }
+    }
+?>

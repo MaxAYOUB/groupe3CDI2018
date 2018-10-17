139898feb2c025b36c6ed5e1929ed831e0b71d4e
31010-52-05 <maxime.ayoub@gmail.com>
2018-10-17 13:26:17 +0200

    le ctrlGen avec fonction

--- groupe3/ctrl/ctrlGeneral.php
+++ groupe3/ctrl/ctrlGeneral.php
@@ -2,9 +2,32 @@
     class ctrlGeneral{
         private $vue;
         private $model;
-        
+
         function __construct(){
             $this->vue=new viewGenerale();
+            $this->model=new modelGeneral();
         }
+
+
+        public function getConsoleAdmin(){
+
+        }
+
+        public function gererAvatar(){
+
+        }
+
+        public function gererCompte(){
+
+        }
+
+        public function gererPassword(){
+
+        }
+
+        private function verifierUser(){
+            
+        }
+    
     }
 ?>

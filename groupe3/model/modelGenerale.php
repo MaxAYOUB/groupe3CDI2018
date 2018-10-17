f072462656031a7227ed2a03a49a12404764e3bf
31010-52-05 <maxime.ayoub@gmail.com>
2018-10-17 13:50:09 +0200

    view/model

---
+++ groupe3/model/modelGenerale.php
@@ -0,0 +1,11 @@
+<?php
+    class modelGeneral{
+        private $dao;
+
+        function __construct(){
+            $this->dao=new viewGenerale();
+        }
+
+    
+    }
+?>
---
+++ groupe3/view/viewGenerale.php
@@ -0,0 +1,29 @@
+<?php
+    class viewGeneral{
+
+        function __construct(){
+        }
+
+
+        public function getConsoleAdmin(){
+
+        }
+
+        public function gererAvatar(){
+
+        }
+
+        public function gererCompte(){
+
+        }
+
+        public function gererPassword(){
+
+        }
+
+        private function verifierUser(){
+            
+        }
+    
+    }
+?>
